bab2c505c1e452d3eb2e921aea971151745e42a3
kWX610740 <konstantin.barzilovich@huawei.com>
2019-01-11 17:02:55 +0300

    Added unit tests for state and for movement

---
+++ tests/test_elevator_basic.py
@@ -0,0 +1,40 @@
+# coding=utf8
+
+import unittest
+from core.elevator import Elevator
+
+class Test_elevator_state(unittest.TestCase):
+    def setUp(self):
+        self.elevator = Elevator(5, 15)
+
+    def test_wait(self):
+        self.elevator.wait(10)
+
+    def test_check_height(self):
+        self.assertEqual(self.elevator.height, 0)
+
+    def test_check_door_close(self):
+        self.assertEqual(self.elevator.inner_door_open, 0)
+
+    def test_check_level(self):
+        self.assertEqual(self.elevator.level, 1)
+
+    def test_check_speed(self):
+        self.assertEqual(self.elevator.speed, 0)
+
+    def test_check_weight(self):
+        self.assertEqual(self.elevator.weight, 0)
+
+    def test_check_buttons(self):
+        self.assertEqual(self.elevator.buttons_pushed, {'inner': [], 'outer': []})
+
+class Test_elevator_movement(unittest.TestCase):
+    def setUp(self):
+        self.elevator = Elevator(5, 15)
+
+    def test_go_to_level(self):
+        self.elevator.wait(10)
+        self.elevator.push_outer(2)
+        self.elevator.wait(100)
+        self.assertEqual(self.elevator.height, 3)
+        self.assertEqual(self.elevator.level, 2)
