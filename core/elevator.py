d1e61fa0116fde3c41c878cd603461c96e5df518
Elena <elena.fedorova@huawei.com>
2019-01-15 20:30:26 +0300

    first attempt

--- core/elevator.py
+++ core/elevator.py
@@ -1,2 +1,30 @@
 # coding=utf8
 
+class Elevator (5, 15):
+
+    def elevator(self):
+        elevator()
+
+    def wait(self, time):
+        elevator.wait()
+
+    def height(self, height_num):
+        elevator.height()
+
+    def inner_door_open(self, num):
+        elevator.inner_door_open()
+
+    def speed(self, speed_status):
+        elevator.speed()
+
+    def level(self, level_num):
+        elevator.level()
+
+    def weight(self, weight_num):
+        elevator.weight()
+
+    def buttons_pushed(self, inner, outer):
+        elevator.buttons_pushed()
+
+    def push_outer(self, num):
+        Elevator.push_outer()
